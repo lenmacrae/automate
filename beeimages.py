0ab62ed8ae2b12e8a5c8e3623d61324541d152c2
lenmacrae <lenmacrae@gmail.com>
2018-01-13 14:26:34 -0400

    refactoring

--- beeimages.py
+++ beeimages.py
@@ -1,19 +1,24 @@
-import os, requests, bs4
+'''download images found at site provided as command line argument'''
 
-res = requests.get('http://beewhimzy.com')
+import os, sys, requests, bs4
+
+sitename = sys.argv[1]
+print(f'Retrieving site: {sitename}')
+res = requests.get(sitename)
 soup = bs4.BeautifulSoup(res.text, 'html.parser')
 images = soup.select('img')
 
 for img in images:
-	imgurl = 'http://beewhimzy.com' + img.get('src')
-	try:
-		result = requests.get(imgurl)
-		imageFile = open(os.path.join('beewhimzy', os.path.basename(imgurl)), 'wb')
-		
-		for chunk in result.iter_content(100000):
-			imageFile.write(chunk)
+    imgurl = 'http://beewhimzy.com' + img.get('src')
+    print(f'Found image: {imgurl}')
+    try:
+        result = requests.get(imgurl)
+        with open(os.path.join('images', os.path.basename(imgurl)), 'wb') \
+        as imageFile:
+            print(f'Saving image')
+            for chunk in result.iter_content(100000):
+                imageFile.write(chunk)
 
-		imageFile.close()
-	except Exception as exc:
-		print(f'There was a problem: {exc}')
+    except Exception as exc:
+        print(f'There was a problem: {exc}')
 
